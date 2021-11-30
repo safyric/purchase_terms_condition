5b5b99db8d2b2ab2979ccb542dc2242d69bcff87
jchanme <39323414+jchanme@users.noreply.github.com>
2021-11-30 12:58:35 +0800

    Update __manifest__.py

--- __manifest__.py
+++ __manifest__.py
@@ -2,8 +2,7 @@
     "name": "Terms and Conditionis Format",
     "version": "12.0.1.1",
     "depends": ['base',
-                'crm',
-                'purchase',
+                'purchase_default_terms_conditions'
                 ],
     'data': [
         'views/purchase_report_templates.xml',
