6f91dd5ea4192882c4754b97ca5eac15cdc66931
jchanme <39323414+jchanme@users.noreply.github.com>
2021-11-30 11:27:10 +0800

    Create __manifest__.py

---
+++ __manifest__.py
@@ -0,0 +1,15 @@
+{
+    "name": "Terms and Conditionis Format",
+    "version": "12.0.1.1",
+    "depends": ['base',
+                'crm',
+                'purchase',
+                ],
+    'data': [
+        'views/purchase_report_templates.xml',
+        'views/purchase.xml',
+    ],
+    'author': 'Safyric Co., Ltd.',
+    'license': 'AGPL-3',
+    'installable': True,
+}
