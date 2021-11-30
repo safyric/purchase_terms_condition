36623cc2da2a2324ea313324fac42baeba3b293d
jchanme <39323414+jchanme@users.noreply.github.com>
2021-11-30 13:16:28 +0800

    Update __manifest__.py

--- __manifest__.py
+++ __manifest__.py
@@ -4,10 +4,6 @@
     "depends": ['purchase',
                 'purchase_default_terms_conditions'
                 ],
-    'data': [
-        'views/purchase_report_templates.xml',
-        'views/purchase.xml',
-    ],
     'author': 'Safyric Co., Ltd.',
     'license': 'AGPL-3',
     'installable': True,
